5f1cdffb51e6e110d892995f4a087f887a85de78
matiaskim <60485887+matiaskim@users.noreply.github.com>
2020-01-30 12:00:09 -0800

    Update hwOne.java

--- HW1/hwOne.java
+++ HW1/hwOne.java
@@ -2,6 +2,6 @@ public class hwOne
 {
      public static void main(String[] args)
      {
-          System.out.println("Hello World");
+          System.out.println("Hello, my name is Matias Kim.");
      }
 }
