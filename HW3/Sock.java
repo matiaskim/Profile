23ee35fa0f37d4d7958c6bb76a06d738ccd767db
matiaskim <60485887+matiaskim@users.noreply.github.com>
2020-02-16 18:05:33 -0800

    Create Sock.java

---
+++ HW3/Sock.java
@@ -0,0 +1,14 @@
+public class Sock
+{
+    private String color;
+    
+    public Sock(String color)
+    {
+        this.color = color;
+    }
+    
+    public String getColor()
+    {
+        return color;
+    }
+}
